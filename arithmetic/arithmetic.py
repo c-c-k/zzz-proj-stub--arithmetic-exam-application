e4106e1f8f18ecb8235a21e8bba36e56fa1e2a88
c-c-k <c-c-k@nym.hush.com>
2023-04-27 09:27:36 +0300

    stage 3 done

--- arithmetic/arithmetic.py
+++ arithmetic/arithmetic.py
@@ -1,25 +1,53 @@
 from operator import add, sub, mul
 from random import randint, choice
 
+class Question:
+    def __init__(self):
+        self.operand_1 = int(randint(2, 9))
+        self.operand_2 = int(randint(2, 9))
+        self.operator_symbol = choice("*+-")
+        self.operator_function = self._get_op_func()
+        self.result = self._calc_solution()
+        
+    def _get_op_func(self):
+        match self.operator_symbol:
+            case "+":
+                operator_function = add
+            case "-":
+                operator_function = sub
+            case "*":
+                operator_function = mul
+        return operator_function
+
+    def _calc_solution(self):
+        return self.operator_function(self.operand_1, self.operand_2)
+
+    def __str__(self):
+        return f"{self.operand_1} {self.operator_symbol} {self.operand_2}"
+        
+
 
 def main():
-    operand_1 = int(randint(2, 9))
-    operand_2 = int(randint(2, 9))
-    operator_symbol = choice("*+-")
+    correct_answers = 0
 
-    match operator_symbol:
-        case "+":
-            op_func = add
-        case "-":
-            op_func = sub
-        case "*":
-            op_func = mul
+    for _ in range(5):
+        question = Question()
+        print(question)
 
-    program_solution = op_func(operand_1, operand_2)
-    print(operand_1, operator_symbol, operand_2, sep=" ")
-    user_sulution = int(input())
-    result = "Right!" if program_solution == user_sulution else "Wrong!"
-    print(result)
+        while True:
+            try:
+                user_sulution = int(input())
+            except ValueError:
+                print("Incorrect format.")
+            else:
+                break
+        if question.result == user_sulution:
+            print("Right!")
+            correct_answers += 1
+        else:
+            print("Wrong!")
+
+    print(f"Your mark is {correct_answers}/5.")
 
 
 if __name__ == "__main__":
