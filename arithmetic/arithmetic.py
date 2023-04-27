d7f84b38ad1378b0f97f37981420b7026b724aeb
c-c-k <c-c-k@nym.hush.com>
2023-04-27 09:27:36 +0300

    stage 1 done

--- arithmetic/arithmetic.py
+++ arithmetic/arithmetic.py
@@ -1,1 +1,20 @@
-# write your code here
+from operator import add, sub, mul
+
+def main():
+    operand_1, operator_symbol, operand_2 = input().split()
+    operand_1 = int(operand_1)
+    operand_2 = int(operand_2)
+
+    match operator_symbol:
+        case "+":
+            op_func = add
+        case "-":
+            op_func = sub
+        case "*":
+            op_func = mul
+
+    print(op_func(operand_1, operand_2))
+
+    
+if __name__ == "__main__":
+    main()
